ad406868f25a8ada22e38853aa81ad8355dc6b30
Ricardo Flamenco <christian.flamenco2028@hilasal.superate.org>
2026-06-17 09:23:22 -0600

    Beta sistema para cambiar de ventanas

--- maker_dicts.py
+++ maker_dicts.py
@@ -4,5 +4,4 @@ from registro import tickets
 
 def tickets_show(parent):
     box_show = tk.Canvas(parent, bg="#303030")
-    frame_tickets = tk.Frame()
     box_show.place(relx=1.0, rely=1.0, anchor="se", width=900, height=600)
