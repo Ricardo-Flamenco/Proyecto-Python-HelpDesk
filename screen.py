a8fec3a6ac079f5f6f2ff6fa020171d23ae31620
Ricardo Flamenco <christian.flamenco2028@hilasal.superate.org>
2026-06-16 19:21:17 -0600

    Haciendo Datos a diccionarios

--- screen.py
+++ screen.py
@@ -1,13 +1,9 @@
 import tkinter as tk
 from tkinter import *
-from create_tickets import *
 
 screen = tk.Tk()
 screen.title("HelpDesk")
 screen.state("zoomed")
-
-button = tk.Button(text="CREAR COSAS", command=lambda:(create(frame)))
-button.pack()
 
 frame = tk.Frame(screen, bg="#1A53CF")
 frame.place(x=50, y=50, width=1000, height=1500)
