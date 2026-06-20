03ac31a975740a4aa1170cae5ce86506c14712c8
Jose-Siciliano <jose.siciliano2028@hilasal.superate.org>
2026-06-20 09:57:30 -0600

    Se adjunto una nueva imagen

--- Proyecto-Python-HelpDesk/screen.py
+++ Proyecto-Python-HelpDesk/screen.py
@@ -5,8 +5,16 @@ screen = tk.Tk()
 screen.title("HelpDesk")
 screen.state("zoomed")
 
-imagen = PhotoImage(file="assets/home.png")
-label = tk.Label(screen, text="HOLA SICI SOS EL G.O.A.T", image=imagen)
-label.pack()
+imagen = PhotoImage(file="Proyecto-Python-HelpDesk/assets/home.png")
+label = tk.Label(screen,text="Home", image=imagen)
+label.pack(side="left")
+imagen.subsample(100, 100)
+
+ajuste = PhotoImage(file="Proyecto-Python-Helpdesk/assets/ajuste.png")
+frame_izq = tk.Frame(screen, bg="#1E293B", width=200, height=300)
+frame_izq.pack(side="left", fill="y")
+
+frame_der = tk.Frame(screen, bg="White", width=400, height=100)
+frame_der.pack(side="top", fill="x")
 
 screen.mainloop()
