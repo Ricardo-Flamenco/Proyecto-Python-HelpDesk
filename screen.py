8fc034b0fc5dcf5fc8e71c6a672dd25fa0079738
Ricardo Flamenco <christian.flamenco2028@hilasal.superate.org>
2026-06-20 18:04:36 -0600

    interfaz basica

--- screen.py
+++ screen.py
@@ -7,11 +7,14 @@ screen = tk.Tk()
 screen.title("HelpDesk")
 screen.state("zoomed")
 
-home = PhotoImage(file="assets/house_solid.png")
-home = home.subsample(36, 36)
+home = PhotoImage(file="assets/house-solid.png")
+home = home.subsample(15, 15)
 
-ajustes = PhotoImage(file="assets/clipboard-list-solid.png")
-ajustes = ajustes.subsample(10, 10)
+clipboard = PhotoImage(file="assets/clipboard-list-solid.png")
+clipboard = clipboard.subsample(15, 15)
+
+glass = PhotoImage(file="assets/magnifying-glass-solid.png")
+glass = glass.subsample(25, 25)
 
 frame_izq = tk.Frame(screen, bg="#1E293B", width=300, height=300)
 frame_izq.pack(side="left", fill="y")
@@ -19,18 +22,45 @@ frame_izq.pack(side="left", fill="y")
 frame_der = tk.Frame(screen, bg="#30a862", width=400, height=100)
 frame_der.pack(side="top", fill="x")
 
-search_bar = tk.Entry(frame_der, bg="#ffffff", width=65, font=("Arial", 12))
-search_bar.place(relx=0.5, y=30, anchor="center")
-
 #importa los frames de las funciones
 menu_frame_show, frame_show = tickets_frame_show(screen)
 main_frame, _, _, _, = register_ticket(screen, frame_show)
 
-#tkraise() levanta los frames encima de otros para hacer un cambio de menu
-register = tk.Button(frame_izq, image=ajustes, bg="#1e293b", compound="left", text="Register", bd=0, command=lambda:main_frame.tkraise())
-register.place(relx=0.5, y=150, anchor="center")
+#FRAME DER
+search_bar = tk.Entry(frame_der, bg="#ffffff", width=45, font=("Arial", 13))
+search_bar.place(relx=0.5, y=30, anchor="center")
 
-tickets_menu = tk.Button(frame_izq, image=home, bg="#1e293b", compound="left", text="Home", bd=0, command=lambda:menu_frame_show.tkraise())
-tickets_menu.place(relx=0.5, y=50, anchor="center")
+glass_icon = tk.Label(frame_der, image=glass ,bg="#30a862")
+glass_icon.place(relx=0.28, y=30, anchor="center")
+
+
+
+#FRAME IZQ
+#Buttons
+active_button = 0
+
+def activate_buttons(active):
+
+    global active_button
+
+    for button in menu_buttons:
+        button.config(bg="#1e293b")
+
+    active_button = active
+
+    if active_button == 0:
+        menu_buttons[0].config(bg="#10164E")
+    elif active_button == 1:
+        menu_buttons[1].config(bg="#10164e")
+
+#tkraise() levanta los frames encima de otros para hacer un cambio de menu
+tickets_menu = tk.Button(frame_izq, image=home, width=300, padx=10, anchor="w", activebackground="#1e293b", bg="#1e293b", fg="#ffffff", compound="left", text="Home", font=("Arial", 15, "bold"), bd=0, command=lambda:(menu_frame_show.tkraise(), activate_buttons(0)))
+tickets_menu.place(x=0, y=50, anchor="w")
+
+register = tk.Button(frame_izq, image=clipboard, width=300, padx=10, anchor="w", activebackground="#1e293b", bg="#1e293b", fg="#ffffff", compound="left", text="Register", font=("Arial", 15, "bold"), bd=0, command=lambda:(main_frame.tkraise(), activate_buttons(1)))
+register.place(x=0, y=120, anchor="w")
+
+#lista que tiene todos los botones de menu
+menu_buttons = [tickets_menu, register]
 
 screen.mainloop()
